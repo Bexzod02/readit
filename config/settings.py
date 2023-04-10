b32d5b3fadf9adf333e4ba7d631f3c6147d68a3f
Bexzod02 <abdumannonovbexzod4@gmail.com>
2023-04-10 22:43:29 +0500

    bexzod

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-kqs#upg!*j_vz-vp$108@&*iaaq)&8u&5zk&av+uhj*b(#fp!)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
